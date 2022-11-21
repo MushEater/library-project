4b2e3c2bea78b8c798abcce73007dafc214704a2
admin <thaordw@gmail.com>
2022-11-21 16:17:21 +0900

    (Refactor): 코드완료

--- library/src/main/java/com/example/library/service/Service.java
+++
@@ -1,5 +0,0 @@
-package com.example.library.service;
-
-public class Service {
-
-}
---
+++ library/src/main/java/com/example/library/service/lbService.java
@@ -0,0 +1,10 @@
+package com.example.library.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class lbService {
+	public void asdsadasd() {
+		
+	}
+}
