545898688f325b853fc3cfee709ae1e48b867275
admin <thaordw@gmail.com>
2022-11-21 16:56:58 +0900

    (Refactor): create UserVO

---
+++ library/src/main/java/com/example/library/vo/UserVO.java
@@ -0,0 +1,9 @@
+package com.example.library.vo;
+
+public class UserVO {
+	private String Name;
+	private String Emailaddress;
+	private String ID;
+	private String PW;
+	
+}
--- library/src/main/java/com/example/library/vo/VO.java
+++ library/src/main/java/com/example/library/vo/VO.java
@@ -1,5 +1,5 @@
 package com.example.library.vo;
 
 public class VO {
-
+	
 }
